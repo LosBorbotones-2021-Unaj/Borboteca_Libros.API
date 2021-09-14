407d3d5aa349e2bf030246c98d363528ac8ccce8
Tomasrojas2001 <tomasrojas20015@gmail.com>
2021-09-14 17:30:26 -0300

    CreacionEntitiesCarroLibroVentas

---
+++ Borboteca_Libros.Domain/Entities/Ventas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borboteca_Libros.Domain.Entities
+{
+    public class Ventas
+    {
+        public int VentaId { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Comprobante { get; set; }
+
+        public string estado { get; set; }
+
+        public int CarroId { get; set; }
+
+        public Carro CarroNavigator { get; set; }
+
+    }
+}
